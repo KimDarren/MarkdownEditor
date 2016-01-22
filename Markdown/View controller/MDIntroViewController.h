707972c10e28caed3317e00694b81ea7ac9e4f9f
KimDarren <korean.darren@gmail.com>
2016-01-22 10:19:02 +0900

    Intro view controller 생성, bitcode disable 등

---
+++ Markdown/View controller/MDIntroViewController.h
@@ -0,0 +1,13 @@
+//
+//  MDIntroViewController.h
+//  Markdown
+//
+//  Created by KimTae jun on 2016. 1. 22..
+//  Copyright © 2016년 Darren Line. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MDIntroViewController : UIViewController
+
+@end
---
+++ Markdown/View controller/MDIntroViewController.m
@@ -0,0 +1,22 @@
+//
+//  MDIntroViewController.m
+//  Markdown
+//
+//  Created by KimTae jun on 2016. 1. 22..
+//  Copyright © 2016년 Darren Line. All rights reserved.
+//
+
+#import "MDIntroViewController.h"
+
+@interface MDIntroViewController ()
+
+@end
+
+@implementation MDIntroViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+@end
